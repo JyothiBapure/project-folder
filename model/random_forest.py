d9222131e4c59247ca1c789ec442c40f274f68b6
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 18:00:32 +0530

    Update random_forest.py

--- model/random_forest.py
+++ model/random_forest.py
@@ -8,9 +8,10 @@ from sklearn.metrics import (
 
 from model.data_utils import load_and_preprocess_data
 
-def run_rf():
+def run_rf(uploaded_test_df=None):
     X_train, X_test, y_train, y_test = load_and_preprocess_data(
         scale_features=False
+        uploaded_test_df=uploaded_test_df
     )
 
     model = RandomForestClassifier(
