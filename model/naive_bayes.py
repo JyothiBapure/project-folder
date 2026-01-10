0261f8561cc09c542260878e1dafda654020187b
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 17:48:36 +0530

    Create naive_bayes.py

---
+++ model/naive_bayes.py
@@ -0,0 +1,40 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import (
+    accuracy_score, roc_auc_score,
+    precision_score, recall_score,
+    f1_score, matthews_corrcoef,
+    confusion_matrix, classification_report
+)
+
+from model.data_utils import load_and_preprocess_data
+
+def run_nb():
+    # GaussianNB does NOT require scaling, but works fine either way
+    X_train, X_test, y_train, y_test = load_and_preprocess_data(
+        scale_features=False
+    )
+
+    # GaussianNB requires dense arrays
+    X_train = X_train.toarray() if hasattr(X_train, "toarray") else X_train
+    X_test = X_test.toarray() if hasattr(X_test, "toarray") else X_test
+
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    probs = model.predict_proba(X_test)[:, 1]
+
+    metrics = {
+        "Accuracy": accuracy_score(y_test, preds),
+        "AUC": roc_auc_score(y_test, probs),
+        "Precision": precision_score(y_test, preds),
+        "Recall": recall_score(y_test, preds),
+        "F1": f1_score(y_test, preds),
+        "MCC": matthews_corrcoef(y_test, preds)
+    }
+
+    return (
+        metrics,
+        confusion_matrix(y_test, preds),
+        classification_report(y_test, preds, output_dict=True)
+    )
