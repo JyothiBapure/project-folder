63f5679925fbfa753d95976ca2f2861c044260ee
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 19:05:20 +0530

    Update naive_bayes.py

--- model/naive_bayes.py
+++ model/naive_bayes.py
@@ -9,7 +9,7 @@ from sklearn.metrics import (
 from model.data_utils import load_and_preprocess_data
 
 def run_nb(uploaded_test_df=None):
-    # GaussianNB does NOT require scaling, but works fine either way
+    
     X_train, X_test, y_train, y_test = load_and_preprocess_data(
         scale_features=False
         uploaded_test_df=uploaded_test_df
@@ -19,23 +19,23 @@ def run_nb(uploaded_test_df=None):
     X_train = X_train.toarray() if hasattr(X_train, "toarray") else X_train
     X_test = X_test.toarray() if hasattr(X_test, "toarray") else X_test
 
-    model = GaussianNB()
-    model.fit(X_train, y_train)
+    naive_model = GaussianNB()
+    naive_model.fit(X_train, y_train)
 
-    preds = model.predict(X_test)
-    probs = model.predict_proba(X_test)[:, 1]
+    y_preds = naive_model.predict(X_test)
+    y_probs = naive_model.predict_proba(X_test)[:, 1]
 
     metrics = {
-        "Accuracy": accuracy_score(y_test, preds),
-        "AUC": roc_auc_score(y_test, probs),
-        "Precision": precision_score(y_test, preds),
-        "Recall": recall_score(y_test, preds),
-        "F1": f1_score(y_test, preds),
-        "MCC": matthews_corrcoef(y_test, preds)
+        "Accuracy": accuracy_score(y_test, y_preds),
+        "AUC": roc_auc_score(y_test, y_probs),
+        "Precision": precision_score(y_test, y_preds),
+        "Recall": recall_score(y_test, y_preds),
+        "F1": f1_score(y_test, y_preds),
+        "MCC": matthews_corrcoef(y_test, y_preds)
     }
 
     return (
         metrics,
-        confusion_matrix(y_test, preds),
-        classification_report(y_test, preds, output_dict=True)
+        confusion_matrix(y_test, y_preds),
+        classification_report(y_test, y_preds, output_dict=True)
     )
