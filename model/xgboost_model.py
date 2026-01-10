8f6a1c85b464e77339e62d2740cb3e01beb05824
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 17:50:04 +0530

    Create xgboost_model.py

---
+++ model/xgboost_model.py
@@ -0,0 +1,46 @@
+from xgboost import XGBClassifier
+from sklearn.metrics import (
+    accuracy_score, roc_auc_score,
+    precision_score, recall_score,
+    f1_score, matthews_corrcoef,
+    confusion_matrix, classification_report
+)
+
+from model.data_utils import load_and_preprocess_data
+
+def run_xgb():
+    X_train, X_test, y_train, y_test = load_and_preprocess_data(
+        scale_features=False
+    )
+
+    model = XGBClassifier(
+        n_estimators=300,
+        max_depth=6,
+        learning_rate=0.1,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        objective="binary:logistic",
+        eval_metric="logloss",
+        random_state=42,
+        n_jobs=-1
+    )
+
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    probs = model.predict_proba(X_test)[:, 1]
+
+    metrics = {
+        "Accuracy": accuracy_score(y_test, preds),
+        "AUC": roc_auc_score(y_test, probs),
+        "Precision": precision_score(y_test, preds),
+        "Recall": recall_score(y_test, preds),
+        "F1": f1_score(y_test, preds),
+        "MCC": matthews_corrcoef(y_test, preds)
+    }
+
+    return (
+        metrics,
+        confusion_matrix(y_test, preds),
+        classification_report(y_test, preds, output_dict=True)
+    )
