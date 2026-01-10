e103f99b04c2fe07718739c8eb3e3fc0234c7b09
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 17:49:26 +0530

    Create random_forest.py

---
+++ model/random_forest.py
@@ -0,0 +1,42 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score, roc_auc_score,
+    precision_score, recall_score,
+    f1_score, matthews_corrcoef,
+    confusion_matrix, classification_report
+)
+
+from model.data_utils import load_and_preprocess_data
+
+def run_rf():
+    X_train, X_test, y_train, y_test = load_and_preprocess_data(
+        scale_features=False
+    )
+
+    model = RandomForestClassifier(
+        n_estimators=200,
+        max_depth=15,
+        min_samples_split=10,
+        random_state=42,
+        n_jobs=-1
+    )
+
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    probs = model.predict_proba(X_test)[:, 1]
+
+    metrics = {
+        "Accuracy": accuracy_score(y_test, preds),
+        "AUC": roc_auc_score(y_test, probs),
+        "Precision": precision_score(y_test, preds),
+        "Recall": recall_score(y_test, preds),
+        "F1": f1_score(y_test, preds),
+        "MCC": matthews_corrcoef(y_test, preds)
+    }
+
+    return (
+        metrics,
+        confusion_matrix(y_test, preds),
+        classification_report(y_test, preds, output_dict=True)
+    )
