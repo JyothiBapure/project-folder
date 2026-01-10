a06c44f841a56c7cfdcdb4373c24426f139b073e
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 17:59:42 +0530

    Update knn.py

--- model/knn.py
+++ model/knn.py
@@ -8,9 +8,10 @@ from sklearn.metrics import (
 
 from model.data_utils import load_and_preprocess_data
 
-def run_knn():
+def run_knn(uploaded_test_df=None):
     X_train, X_test, y_train, y_test = load_and_preprocess_data(
         scale_features=True
+        uploaded_test_df=uploaded_test_df
     )
 
     model = KNeighborsClassifier(
