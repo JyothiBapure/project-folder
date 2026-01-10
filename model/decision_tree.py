61a1ebb4787e8d8182e253959c34ce88b7aeda31
JyothiBapure <jyothi.patil4@gmail.com>
2026-01-10 18:56:04 +0530

    Update decision_tree.py

--- model/decision_tree.py
+++ model/decision_tree.py
@@ -6,37 +6,37 @@ from sklearn.metrics import (
     confusion_matrix, classification_report
 )
 
-from model.data_utils import load_and_preprocess_data
+from model.data_utils import load_train_and_test_data
 
-def run_dt(uploaded_test_df=None):
-    X_train, X_test, y_train, y_test = load_and_preprocess_data(
-        scale_features=False
+def run_logic(uploaded_test_df=None):
+    X_train, X_test, y_train, y_test = load_train_and_test_data(
+        scale_features=True,
         uploaded_test_df=uploaded_test_df
     )
 
-    model = DecisionTreeClassifier(
+    decision_model = DecisionTreeClassifier(
         criterion="gini",
         max_depth=10,
         min_samples_split=20,
         random_state=42
     )
 
-    model.fit(X_train, y_train)
+    decision_model.fit(X_train, y_train)
 
-    preds = model.predict(X_test)
-    probs = model.predict_proba(X_test)[:, 1]
+    y_preds = decision_modell.predict(X_test)
+    y_probs = decision_model.predict_proba(X_test)[:, 1]
 
     metrics = {
-        "Accuracy": accuracy_score(y_test, preds),
-        "AUC": roc_auc_score(y_test, probs),
-        "Precision": precision_score(y_test, preds),
-        "Recall": recall_score(y_test, preds),
-        "F1": f1_score(y_test, preds),
-        "MCC": matthews_corrcoef(y_test, preds)
+        "Accuracy": accuracy_score(y_test, y_preds),
+        "AUC": roc_auc_score(y_test, y_probs),
+        "Precision": precision_score(y_test, y_preds),
+        "Recall": recall_score(y_test, y_preds),
+        "F1": f1_score(y_test, y_preds),
+        "MCC": matthews_corrcoef(y_test, y_preds)
     }
 
     return (
         metrics,
-        confusion_matrix(y_test, preds),
-        classification_report(y_test, preds, output_dict=True)
+        confusion_matrix(y_test, y_preds),
+        classification_report(y_test, y_preds, output_dict=True)
     )
