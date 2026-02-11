7c8713efd434b44c35adc7832dd12962355e64f6
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-11 21:14:46 +0530

    Update decision_tree.py

--- model/decision_tree.py
+++ model/decision_tree.py
@@ -1,52 +1,38 @@
-import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import (
-    accuracy_score, roc_auc_score, precision_score, 
-    recall_score, f1_score, matthews_corrcoef, 
-    confusion_matrix, classification_report, precision_recall_curve
+    accuracy_score, roc_auc_score,
+    precision_score, recall_score,
+    f1_score, matthews_corrcoef,
+    confusion_matrix, classification_report
 )
+
 from model.data_utils import load_train_and_test_data
 
-
 def run_dt(uploaded_test_df=None):
-    X_train, X_val, X_test, y_train, y_val, y_test = load_train_and_test_data(
-        scale_features=True,
+
+    X_train, X_test, y_train, y_test = load_train_and_test_data(
+        scale_features=False,
         uploaded_test_file=uploaded_test_df
     )
-    decision_model = DecisionTreeClassifier(
+
+    model = DecisionTreeClassifier(
         max_depth=8,
         criterion='entropy',
-        min_samples_leaf=20,
-        min_samples_split=50,
-        class_weight=None,
         random_state=42
     )
-    #decision_model = DecisionTreeClassifier(criterion = 'entropy')
-    decision_model.fit(X_train, y_train)
-    y_val_probs = decision_model.predict_proba(X_val)[:, 1]
 
-    precisions, recalls, thresholds = precision_recall_curve(y_val, y_val_probs)
-    f1_scores = (2 * precisions * recalls) / (precisions + recalls + 1e-8)
-    optimal_idx = np.argmax(f1_scores)
-    best_threshold = thresholds[optimal_idx]
+    model.fit(X_train, y_train)
 
-    # Evaluate on test
-    y_probs = decision_model.predict_proba(X_test)[:, 1]
-    y_preds = (y_probs >= best_threshold).astype(int)
+    y_preds = model.predict(X_test)
+    y_probs = model.predict_proba(X_test)[:, 1]
 
     metrics = {
         "Accuracy": round(accuracy_score(y_test, y_preds), 4),
         "AUC": round(roc_auc_score(y_test, y_probs), 4),
         "Precision": round(precision_score(y_test, y_preds, zero_division=0), 4),
         "Recall": round(recall_score(y_test, y_preds, zero_division=0), 4),
-        "F1": round(f1_score(y_test, y_preds, zero_division=0), 4),
-        "MCC": round(matthews_corrcoef(y_test, y_preds), 4),
-        
+        "F1 Score": round(f1_score(y_test, y_preds, zero_division=0), 4),
+        "MCC": round(matthews_corrcoef(y_test, y_preds), 4)
     }
-   
-    return (
-        metrics,
-        confusion_matrix(y_test, y_preds),
-        classification_report(y_test, y_preds, output_dict=True)
-    )
+
+    return metrics, confusion_matrix(y_test, y_preds), classification_report(y_test, y_preds, output_dict=True)
