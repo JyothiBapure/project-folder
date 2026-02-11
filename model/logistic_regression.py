24b546f1fe3619cb2150d80f4d3516c14c6e4112
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-11 23:15:07 +0530

    Update logistic_regression.py

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -24,7 +24,7 @@ def get_metrics(y_true, y_probs):
     }
 
 
-def run_logistic(uploaded_test_df=None):
+def run_logic(uploaded_test_df=None):
     X_train, X_test, y_train, y_test = load_train_and_test_data(scale_features=True, uploaded_test_file=uploaded_test_df)
     model = LogisticRegression(class_weight='balanced', max_iter=1000, random_state=42)
     model.fit(X_train, y_train)
