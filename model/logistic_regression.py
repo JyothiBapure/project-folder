ad7d06b5566ea001f36728a77e4f241f36344c5a
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-11 21:14:23 +0530

    Update logistic_regression.py

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -9,28 +9,25 @@ from sklearn.metrics import (
 from model.data_utils import load_train_and_test_data
 
 def run_logic(uploaded_test_df=None):
+
     X_train, X_test, y_train, y_test = load_train_and_test_data(
         scale_features=True,
         uploaded_test_file=uploaded_test_df
     )
 
-    logistic_model = LogisticRegression()
-    logistic_model.fit(X_train, y_train)
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
 
-    y_preds = logistic_model.predict(X_test)
-    y_probs = logistic_model.predict_proba(X_test)[:, 1]
+    y_preds = model.predict(X_test)
+    y_probs = model.predict_proba(X_test)[:, 1]
 
     metrics = {
         "Accuracy": round(accuracy_score(y_test, y_preds), 4),
         "AUC": round(roc_auc_score(y_test, y_probs), 4),
-        "Precision": round(precision_score(y_test, y_preds), 4),
-        "Recall": round(recall_score(y_test, y_preds), 4),
-        "F1": round(f1_score(y_test, y_preds), 4),
+        "Precision": round(precision_score(y_test, y_preds, zero_division=0), 4),
+        "Recall": round(recall_score(y_test, y_preds, zero_division=0), 4),
+        "F1 Score": round(f1_score(y_test, y_preds, zero_division=0), 4),
         "MCC": round(matthews_corrcoef(y_test, y_preds), 4)
     }
 
-    return (
-        metrics,
-        confusion_matrix(y_test, y_preds),
-        classification_report(y_test, y_preds, output_dict=True)
-    )
+    return metrics, confusion_matrix(y_test, y_preds), classification_report(y_test, y_preds, output_dict=True)
