1657170e793a2237c86c61b88f72ac02b60d5801
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-11 23:49:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-from model.data_util import load_training_data, load_test_data, preprocess
+from model.data_utils import load_training_data, load_test_data, preprocess
 
 from model import (
     logistic_model,
