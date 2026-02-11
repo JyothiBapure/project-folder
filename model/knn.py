e1bdabf5f7ab172674f77cc0bd1eb83e32c18f24
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-11 12:23:27 +0530

    Update knn.py

--- model/knn.py
+++ model/knn.py
@@ -21,16 +21,21 @@ def run_knn(uploaded_test_df=None):
 
     knn_model.fit(X_train, y_train)
 
-    y_preds = knn_model.predict(X_test)
-    y_probs = knn_model.predict_proba(X_test)[:, 1]
+    #y_preds = knn_model.predict(X_test)
+    #y_probs = knn_model.predict_proba(X_test)[:, 1]
+    y_probs = model.predict_proba(X_test)[:, 1]
+
+    threshold = 0.4   # keep same threshold for fair comparison
+    y_preds = (y_probs >= threshold).astype(int)
+
 
     metrics = {
-        "Accuracy": round(accuracy_score(y_test, y_preds), 4),
-        "AUC": round(roc_auc_score(y_test, y_probs), 4),
-        "Precision": round(precision_score(y_test, y_preds), 4),
-        "Recall": round(recall_score(y_test, y_preds), 4),
-        "F1": round(f1_score(y_test, y_preds), 4),
-        "MCC": round(matthews_corrcoef(y_test, y_preds), 4)
+        "Accuracy": round(accuracy_score(y_test, y_preds, zero_division=0), 4),
+        "AUC": round(roc_auc_score(y_test, y_probs, zero_division=0), 4),
+        "Precision": round(precision_score(y_test, y_preds, zero_division=0), 4),
+        "Recall": round(recall_score(y_test, y_preds, zero_division=0), 4),
+        "F1": round(f1_score(y_test, y_preds, zero_division=0), 4),
+        "MCC": round(matthews_corrcoef(y_test, y_preds, zero_division=0), 4)
     }
 
     return (
