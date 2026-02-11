967cecb1f221b6973d859e9b0d709df858d19704
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-11 12:16:21 +0530

    Update data_utils.py

--- model/data_utils.py
+++ model/data_utils.py
@@ -9,18 +9,19 @@ column_names = [
     'native-country', 'income'
 ]
 
+def map_income(x):
+    x = str(x).strip()
+    if x in ['<=50K', '50K', '<=50K.']:
+        return 0
+    elif x in ['>50K', '>50K.']:
+        return 1
+    else:
+        raise ValueError(f"Unknown income value: {x}")
+
 def preprocess_data(data1, feature_columns=None, has_label=True):
     data1 = data1.replace('?', pd.NA).dropna()
     
     if has_label:
-        def map_income(x):
-            x = str(x).strip()
-            if x in ['<=50K', '50K', '<=50K.']:
-                return 0
-            elif x in ['>50K', '>50K.']:
-                return 1
-            else:
-                raise ValueError(f"Unknown income value: {x}")
         y = data1['income'].apply(map_income)
         X = data1.drop(columns=['income'], errors='ignore')
     else:
@@ -42,7 +43,8 @@ def load_train_and_test_data(scale_features=False, uploaded_test_file=None):
 
     if uploaded_test_file is None:
         X = data.drop('income', axis=1)
-        y = data['income'].apply(lambda x: 0 if str(x).strip() in ['<=50K','50K'] else 1)
+        #y = data['income'].apply(lambda x: 0 if str(x).strip() in ['<=50K','50K'] else 1)
+        y = data['income'].apply(map_income)
         X_encoded = pd.get_dummies(X, drop_first=True)
         X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42, stratify=y)
     else:
