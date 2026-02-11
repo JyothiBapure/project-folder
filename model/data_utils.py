851e79d901e2a971692912ed9807f01975707469
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-12 00:12:04 +0530

    Update data_utils.py

--- model/data_utils.py
+++ model/data_utils.py
@@ -1,7 +1,6 @@
 # data_util.py
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 COLUMN_NAMES = [
@@ -11,8 +10,7 @@ COLUMN_NAMES = [
     "income"
 ]
 
-
-def load_training_data(train_path="data/adult.data"):
+def load_training_data(train_path="adult.data"):
     df = pd.read_csv(train_path, header=None, names=COLUMN_NAMES, na_values=" ?")
     df.dropna(inplace=True)
     df["income"] = df["income"].str.replace(".", "", regex=False)
@@ -26,51 +24,24 @@ def load_test_data(uploaded_file):
     return df
 
 
-def encode_data(train_df, test_df=None):
-
+def preprocess(train_df, test_df):
     train_df = train_df.copy()
+    test_df = test_df.copy()
 
     categorical_cols = train_df.select_dtypes(include=["object"]).columns
+
     label_encoders = {}
 
     for col in categorical_cols:
         le = LabelEncoder()
         train_df[col] = le.fit_transform(train_df[col])
+        test_df[col] = le.transform(test_df[col])
         label_encoders[col] = le
 
-    if test_df is not None:
-        test_df = test_df.copy()
-        for col in categorical_cols:
-            test_df[col] = label_encoders[col].transform(test_df[col])
+    X_train = train_df.drop("income", axis=1)
+    y_train = train_df["income"]
 
-    return train_df, test_df
-
-
-def get_data(uploaded_file=None):
-
-    train_df = load_training_data()
-
-    if uploaded_file is not None:
-        # External test file mode
-        test_df = load_test_data(uploaded_file)
-
-        train_df, test_df = encode_data(train_df, test_df)
-
-        X_train = train_df.drop("income", axis=1)
-        y_train = train_df["income"]
-
-        X_test = test_df.drop("income", axis=1)
-        y_test = test_df["income"]
-
-    else:
-        # Internal split mode
-        train_df, _ = encode_data(train_df)
-
-        X = train_df.drop("income", axis=1)
-        y = train_df["income"]
-
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42, stratify=y
-        )
+    X_test = test_df.drop("income", axis=1)
+    y_test = test_df["income"]
 
     return X_train, X_test, y_train, y_test
