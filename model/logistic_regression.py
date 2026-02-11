327e45ca4a0da856713e12555cfcaa0726a44832
JyothiBapure <jyothi.patil4@gmail.com>
2026-02-12 00:07:21 +0530

    Update logistic_regression.py

--- model/logistic_regression.py
+++ model/logistic_regression.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score, matthews_corrcoef
 
 def train_and_evaluate(X_train, X_test, y_train, y_test):
-    model = LogisticRegression(max_iter=1000)
+    model = LogisticRegression(max_iter=1000, class_weight='balanced')
 
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
